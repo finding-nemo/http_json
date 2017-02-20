bcac0b0077733549143789ef496a6063def184d2
Firefly4266 <johnathon@Icon-Base.(none)>
2017-02-19 21:41:40 -0800

    new start

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  fs.createReadStream(__dirname + '/index.htm', 'utf8').pipe(res);
+}).listen(1335, '127.0.0.1');
